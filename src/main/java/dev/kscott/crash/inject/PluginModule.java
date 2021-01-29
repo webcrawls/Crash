c264a7c5758bec566124cb5e1a9946c122c75a43
kadenscott <kscottdev@gmail.com>
2021-01-29 11:14:03 -0500

    construct Guice injector

--- src/main/java/dev/kscott/crash/inject/PluginModule.java
+++ src/main/java/dev/kscott/crash/inject/PluginModule.java
@@ -1,7 +1,7 @@
 package dev.kscott.crash.inject;
 
 import com.google.inject.AbstractModule;
-import dev.kscott.crash.CrashPlugin;
+import dev.kscott.casino.CasinoPlugin;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
@@ -14,25 +14,25 @@ public class PluginModule extends AbstractModule {
     /**
      * CrashPlugin reference.
      */
-    private final @NonNull CrashPlugin plugin;
+    private final @NonNull CasinoPlugin plugin;
 
     /**
      * Constructs PluginModule.
      *
      * @param plugin CrashPlugin reference.
      */
-    public PluginModule(final @NonNull CrashPlugin plugin) {
+    public PluginModule(final @NonNull CasinoPlugin plugin) {
         this.plugin = plugin;
     }
 
     /**
-     * Configures the {@link PluginModule} to bind {@link Plugin}, {@link JavaPlugin}, and {@link CrashPlugin} to {@link this#plugin)}.
+     * Configures the {@link PluginModule} to bind {@link Plugin}, {@link JavaPlugin}, and {@link CasinoPlugin} to {@link this#plugin)}.
      */
     @Override
     public void configure() {
         this.bind(Plugin.class).toInstance(plugin);
         this.bind(JavaPlugin.class).toInstance(plugin);
-        this.bind(CrashPlugin.class).toInstance(plugin);
+        this.bind(CasinoPlugin.class).toInstance(plugin);
     }
 
 }
