2368cb5396222b7bbf78340e1c7dbe85cb72c991
kadenscott <kscottdev@gmail.com>
2021-01-29 10:57:40 -0500

    make CasinoPlugin nicer

--- src/main/java/dev/kscott/casino/CasinoPlugin.java
+++ src/main/java/dev/kscott/casino/CasinoPlugin.java
@@ -1,17 +1,6 @@
 package dev.kscott.casino;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import dev.kscott.crash.Metrics;
-import dev.kscott.crash.command.CrashAdminCommand;
-import dev.kscott.crash.command.CrashCommand;
-import dev.kscott.crash.inject.CommandModule;
-import dev.kscott.crash.inject.ConfigModule;
-import dev.kscott.crash.inject.GameModule;
-import dev.kscott.crash.inject.PluginModule;
-import dev.kscott.crash.listeners.InventoryCloseListener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.checkerframework.checker.nullness.qual.NonNull;
 
 /**
  * The Casino plugin main class.
@@ -22,18 +11,25 @@ public class CasinoPlugin extends JavaPlugin {
      * If true, this server is Spigot or CB.
      * If false, it's something else.
      */
-    public static boolean IS_DEPRECATED = false;
+    private static boolean DEPRECATED = false;
 
     /**
      * Constructs the injector and initializes some stuff.
      */
     @Override
     public void onEnable() {
-        IS_DEPRECATED = this.getServer().getVersion().contains("Spigot") || this.getServer().getVersion().contains("Craftbukkit");
+        DEPRECATED = this.getServer().getVersion().contains("Spigot") || this.getServer().getVersion().contains("Craftbukkit");
+
         if (this.getServer().getPluginManager().getPlugin("Vault") == null) {
             throw new RuntimeException("The Vault plugin is not installed. Please install it!");
         }
     }
 
-
+    /**
+     * @return {@code true} if this server is running Spigot or CraftBukkit. If {@code false}, this server is likely
+     * running Paper, or a fork of Paper.
+     */
+    public static boolean isDeprecated() {
+        return DEPRECATED;
+    }
 }
