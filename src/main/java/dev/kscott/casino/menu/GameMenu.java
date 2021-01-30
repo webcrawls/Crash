22f3c33c25fbaa1e7ec6a3ad151f96a3ca738c18
kadenscott <kscottdev@gmail.com>
2021-01-29 19:51:29 -0500

    add javadocs to GameMenu

--- src/main/java/dev/kscott/casino/menu/GameMenu.java
+++ src/main/java/dev/kscott/casino/menu/GameMenu.java
@@ -5,12 +5,22 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 
 /**
  * A {@link Menu} type that has access to it's related {@link Game}.
- * @param <T>
+ *
+ * @param <T> {@link Game} associated with this {@link GameMenu}.
  */
 public abstract class GameMenu<T extends Game> extends Menu {
 
+    /**
+     * Reference to the {@link Game}.
+     */
     protected final @NonNull T game;
 
+    /**
+     * Constructs {@link GameMenu}.
+     *
+     * @param id   id of this menu.
+     * @param game the {@link Game} associated with this menu.
+     */
     public GameMenu(
             final @NonNull String id,
             final @NonNull T game
