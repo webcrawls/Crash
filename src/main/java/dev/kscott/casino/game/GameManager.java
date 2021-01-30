e8aea9e44ca84b1c8d5ba6675c8784d0b3bba495
kadenscott <kscottdev@gmail.com>
2021-01-29 19:24:33 -0500

    run TickingGames on startup

--- src/main/java/dev/kscott/casino/game/GameManager.java
+++ src/main/java/dev/kscott/casino/game/GameManager.java
@@ -2,6 +2,7 @@ package dev.kscott.casino.game;
 
 import com.google.inject.Inject;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
@@ -24,6 +25,11 @@ public class GameManager {
     private final @NonNull JavaPlugin plugin;
 
     /**
+     * Maps a {@link TickingGame} to it's {@link BukkitRunnable}.
+     */
+    private final Map<TickingGame, BukkitRunnable> tickMap;
+
+    /**
      * Constructs {@link GameManager}.
      *
      * @param plugin {@link JavaPlugin} reference.
@@ -32,16 +38,35 @@ public class GameManager {
     public GameManager(final @NonNull JavaPlugin plugin) {
         this.gameMap = new HashMap<>();
         this.plugin = plugin;
+        this.tickMap = new HashMap<>();
     }
 
     /**
      * Registers a {@link Game} and runs it's setup method.
+     * <p>
+     * If {@code game} is a {@link TickingGame}, the runnable code will be initialized.
      *
      * @param game the {@link Game} to register.
      */
     public void registerGame(final @NonNull Game game) {
         this.gameMap.put(game.getGameType(), game);
         game.setup();
+
+        if (game instanceof TickingGame) {
+            final @NonNull TickingGame tickingGame = (TickingGame) game;
+
+            final int tickTime = tickingGame.getTickSpeed();
+
+            final @NonNull BukkitRunnable runnable = new BukkitRunnable() {
+                @Override
+                public void run() {
+                    tickingGame.runGameTick();
+                };
+            };
+
+            tickingGame.startGame();
+            runnable.runTaskTimer(plugin, 0, tickTime);
+        }
     }
 
     /**
