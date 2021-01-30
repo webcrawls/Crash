2c9de8b156ffce0ca09e8c3358497834c520c20d
kadenscott <kscottdev@gmail.com>
2021-01-30 00:38:06 -0500

    add MenuManager methods to access MenuManager

--- src/main/java/dev/kscott/casino/menu/MenuManager.java
+++ src/main/java/dev/kscott/casino/menu/MenuManager.java
@@ -19,6 +19,11 @@ public class MenuManager {
     private final @NonNull Map<GameType, Map<String, Menu>> gameMenuMap;
 
     /**
+     * Ties {@link GameType}s to a {@link MenuProvider}.
+     */
+    private final @NonNull Map<GameType, MenuProvider> providerMap;
+
+    /**
      * The menu type for no menu.
      */
     public static final @NonNull String MT_NONE = "none";
@@ -28,6 +33,17 @@ public class MenuManager {
      */
     public MenuManager() {
         this.gameMenuMap = new HashMap<>();
+        this.providerMap = new HashMap<>();
+    }
+
+    /**
+     * Registers a {@link MenuProvider}.
+     *
+     * @param gameType     {@link GameType} to associate this {@link MenuProvider} with.
+     * @param menuProvider {@link MenuProvider} instance.
+     */
+    public void registerProvider(final @NonNull GameType gameType, final @NonNull MenuProvider menuProvider) {
+        this.providerMap.put(gameType, menuProvider);
     }
 
     /**
@@ -65,6 +81,44 @@ public class MenuManager {
         final @NonNull Map<String, Menu> map = this.gameMenuMap.get(gameType);
 
         return map.get(id);
+    }
+
+    /**
+     * Returns a {@link Menu} with the given {@link GameType}, accessing the {@link MenuProvider} associated with {@code gameType}.
+     *
+     * @param gameType {@link GameType} of the {@link MenuProvider}.
+     * @return {@link Menu}. May be null if no provider was found, or no menu was returned.
+     */
+    public @Nullable Menu getMenu(final @NonNull GameType gameType) {
+        final @Nullable MenuProvider menuProvider = this.providerMap.get(gameType);
+
+        if (menuProvider == null) {
+            return null;
+        }
+
+        final @NonNull String menuId = menuProvider.getMenuId();
+
+        final @Nullable Menu menu = this.getMenu(gameType, menuId);
+
+        return menu;
+    }
+
+    /**
+     * Opens a menu for a {@link Player}, using the given {@link GameType}.
+     * <p>
+     * If the menu is null, nothing will be opened.
+     *
+     * @param player   {@link Player} to open menu for.
+     * @param gameType the {@link GameType} associated with this {@link Menu}.
+     */
+    public void openMenu(final @NonNull Player player, final @NonNull GameType gameType) {
+        final @Nullable Menu menu = this.getMenu(gameType);
+
+        if (menu == null) {
+            return;
+        }
+
+        menu.showMenu(player);
     }
 
 }
