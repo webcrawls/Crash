59219555bbc074fead9b49c8820a6424a95e06eb
kadenscott <kscottdev@gmail.com>
2021-01-30 03:09:26 -0500

    do menu shenanigans

--- src/main/java/dev/kscott/casino/game/crash/menu/CrashPreGameMenu.java
+++ src/main/java/dev/kscott/casino/game/crash/menu/CrashPreGameMenu.java
@@ -1,10 +1,14 @@
 package dev.kscott.casino.game.crash.menu;
 
+import com.github.stefvanschie.inventoryframework.gui.GuiItem;
 import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
 import com.github.stefvanschie.inventoryframework.gui.type.util.Gui;
+import com.github.stefvanschie.inventoryframework.pane.StaticPane;
 import dev.kscott.casino.game.GameType;
 import dev.kscott.casino.game.crash.CrashGame;
 import dev.kscott.casino.menu.GameMenu;
+import dev.kscott.crash.utils.ItemBuilder;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
@@ -30,6 +34,19 @@ public class CrashPreGameMenu extends GameMenu<CrashGame> {
     @Override
     public Gui constructMenu(final @NonNull Player player) {
         final @NonNull ChestGui gui = new ChestGui(6, "Crash Pre Game "+this.game.getPreGameSeconds()+"s");
+
+        gui.setOnGlobalClick(event -> event.setCancelled(true));
+
+        final @NonNull StaticPane bgPane = new StaticPane(9, 6);
+        bgPane.fillWith(new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).name("").build());
+
+        final @NonNull StaticPane fgPane = new StaticPane(4, 2, 1, 1);
+        fgPane.addItem(new GuiItem(new ItemBuilder(Material.RED_STAINED_GLASS_PANE)
+                .name(this.game.getPreGameSecondsLeft()+"s until start")
+                .build()), 0, 0);
+
+        gui.addPane(fgPane);
+        gui.addPane(bgPane);
 
         return gui;
     }
--- src/main/java/dev/kscott/casino/game/crash/menu/CrashRunningMenu.java
+++ src/main/java/dev/kscott/casino/game/crash/menu/CrashRunningMenu.java
@@ -1,10 +1,14 @@
 package dev.kscott.casino.game.crash.menu;
 
+import com.github.stefvanschie.inventoryframework.gui.GuiItem;
 import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
 import com.github.stefvanschie.inventoryframework.gui.type.util.Gui;
+import com.github.stefvanschie.inventoryframework.pane.StaticPane;
 import dev.kscott.casino.game.GameType;
 import dev.kscott.casino.game.crash.CrashGame;
 import dev.kscott.casino.menu.GameMenu;
+import dev.kscott.crash.utils.ItemBuilder;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
@@ -30,7 +34,20 @@ public class CrashRunningMenu extends GameMenu<CrashGame> {
      */
     @Override
     public Gui constructMenu(final @NonNull Player player) {
-        final @NonNull ChestGui gui = new ChestGui(6, "Crash Running " + this.game.getCurrentMultiplier() + "s");
+        final @NonNull ChestGui gui = new ChestGui(6, "Crash");
+
+        gui.setOnGlobalClick(event -> event.setCancelled(true));
+
+        final @NonNull StaticPane bgPane = new StaticPane(9, 6);
+        bgPane.fillWith(new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).name("").build());
+
+        final @NonNull StaticPane fgPane = new StaticPane(4, 2, 1, 1);
+        fgPane.addItem(new GuiItem(new ItemBuilder(Material.GREEN_STAINED_GLASS_PANE)
+                .name(this.game.getCurrentMultiplier()+"x")
+                .build()), 0, 0);
+
+        gui.addPane(fgPane);
+        gui.addPane(bgPane);
 
         return gui;
     }
